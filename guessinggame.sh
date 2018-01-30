594dc7991d3f99a85e7134707cca9317b1bd0ad1
jye56 <jye56@yahoo.com>
2018-01-30 08:55:48 -0500

    first commit for guessing game program

---
+++ guessinggame.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+# define a function to get the number of files in the current directory
+function getNumfiles {
+	ls -l | wc -l > numoffiles.txt
+	numf=$(egrep "^[0-9]+" numoffiles.txt)
+	rm numoffiles.txt
+	echo $numf
+}
+
+# main program
+
+# get the number of files in the current directory by calling function getNumfiles
+numfiles=$(getNumfiles)
+
+# while loop for the interactive guessing
+while [[ $response -ne $numfiles ]]
+do
+	echo "Please guess the number of files in the directory: "
+	read response
+
+	if [[ $response -gt $numfiles ]]
+	then
+		echo "The guess is too high"
+	elif [[ $response -lt $numfiles ]]
+	then
+		echo "The guess is too low"
+	else 
+		echo "You are correct!!!"
+	fi
+done
+
+
+
---
+++ readme.txt
@@ -0,0 +1,4 @@
+This is an interactive program for guessing he number of files in a directory
+Tue Jan 30 08:49:58 EST 2018
+the number of the lines of the program is:
+35
